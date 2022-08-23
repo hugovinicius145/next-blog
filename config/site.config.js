53ce3c821a028a0aec1e97cc70d3586e20965ce3
hugo vinicius <hugovinicius145@gmail.com>
2022-08-23 17:43:31 -0300

    Iniciando mudancas

--- config/site.config.js
+++ config/site.config.js
@@ -1,70 +1,70 @@
 const siteConfig = {
   seo: {
-    title: 'itsrakesh - blog',
-    author: 'Rakesh Potnuru',
-    copyright: 'itsrakesh',
+    title: 'Blog HL Digital',
+    author: 'Hugo Lima',
+    copyright: 'hugoviniccius',
     description:
       'Browse programming tutorials and articles written by Rakesh Potnuru. Learn Web Development, Blockchain, Web3 and Technical writing.',
     image:
       'https://ik.imagekit.io/itsrakesh/Blog/itsrakesh_-_blog_r0SAC1__p.png?ik-sdk-version=javascript-1.4.3&updatedAt=1656350838485',
-    altText: 'itsrakesh - blog',
+    altText: 'Blog HL Digital',
     ogType: 'website',
     monetizationTag: '$ilp.uphold.com/82eq8NKY4NEZ',
-    twitterUsername: '@rakesh_at_tweet',
+    // twitterUsername: '@rakesh_at_tweet',
     pages: {
       supportMe: {
-        title: 'Support Me | itsrakesh - blog',
+        title: 'Support Me | Blog HL Digital',
         description:
           'Your support keeps this blog alive and motivates me to keep creating content.',
         image:
           'https://ik.imagekit.io/itsrakesh/Blog/support_me_DUCAFlO96.png?ik-sdk-version=javascript-1.4.3&updatedAt=1656350841027'
       },
       articles: {
-        title: 'Articles | itsrakesh - blog',
+        title: 'Articles | Blog HL Digital',
         description:
           'Read articles on web development, web3, blockchain, design, and more.',
         image:
           'https://ik.imagekit.io/itsrakesh/Blog/articles_QqdeLjlbl.png?ik-sdk-version=javascript-1.4.3&updatedAt=1656350838382'
       },
       snippets: {
-        title: 'Snippets | itsrakesh - blog',
+        title: 'Snippets | Blog HL Digital',
         description:
           'Snippets are code samples and quick tips that I have written. Explore and share them.',
         image:
           'https://ik.imagekit.io/itsrakesh/Blog/snippets_ma1o5jx8Z.png?ik-sdk-version=javascript-1.4.3&updatedAt=1656350839459'
       },
       categories: {
-        title: 'Categories | itsrakesh - blog',
+        title: 'Categories | Blog HL Digital',
         description: 'Browse different categories.',
         image:
           'https://ik.imagekit.io/itsrakesh/Blog/categories_u4RkWWNIi.png?ik-sdk-version=javascript-1.4.3&updatedAt=1656350838394'
       },
       tags: {
-        title: 'Tags | itsrakesh - blog',
+        title: 'Tags | Blog HL Digital',
         description: 'Explore all the tags.',
         image:
           'https://ik.imagekit.io/itsrakesh/Blog/tags_kBWiaEWuR.png?ik-sdk-version=javascript-1.4.3&updatedAt=1656350842330'
       },
       privacy: {
-        title: 'Privacy Policy | itsrakesh - blog',
-        description: 'This page contains itsrakesh - blog privacy policies.',
+        title: 'Privacy Policy | Blog HL Digital',
+        description: 'This page contains Blog HL Digital privacy policies.',
         image:
           'https://ik.imagekit.io/itsrakesh/Blog/privacy_policy_xq4AJG1mUd.png?ik-sdk-version=javascript-1.4.3&updatedAt=1656350838506'
       },
       cookie: {
-        title: 'Cookie Policy | itsrakesh - blog',
-        description: 'This page contains itsrakesh - blog privacy policies.',
+        title: 'Cookie Policy | Blog HL Digital',
+        description: 'This page contains Blog HL Digital privacy policies.',
         image:
           'https://ik.imagekit.io/itsrakesh/Blog/cookie_policy_ulfXI9cMG.png?ik-sdk-version=javascript-1.4.3&updatedAt=1656351873708'
       },
       terms: {
-        title: 'Terms & Conditions | itsrakesh - blog',
-        description: 'This page contains itsrakesh - blog terms & conditions.',
+        title: 'Terms & Conditions | Blog HL Digital',
+        description: 'This page contains Blog HL Digital terms & conditions.',
         image:
           'https://ik.imagekit.io/itsrakesh/Blog/terms___conditions_jb7mesw-j.png?ik-sdk-version=javascript-1.4.3&updatedAt=1656350842954'
       },
       externalArticles: {
-        title: 'External Articles | itsrakesh - blog',
+        title: 'External Articles | Blog HL Digital',
         description: 'Read articles written by Rakesh Potnuru on other sites.',
         image:
           'https://ik.imagekit.io/itsrakesh/Blog/external_articles_urr6u2vH1.png?ik-sdk-version=javascript-1.4.3&updatedAt=1657081755552'
@@ -93,15 +93,13 @@ const siteConfig = {
   },
   urls: {
     socials: {
-      twitter: 'https://twitter.com/rakesh_at_tweet',
-      linkedin: 'https://www.linkedin.com/in/itsrakesh',
-      github: 'https://github.com/RakeshPotnuru',
-      showwcase: 'https://www.showwcase.com/itsrakesh'
+      linkedin: 'https://www.linkedin.com/in/hugoviniccius/',
+      github: 'https://github.com/hugovinicius145',
     },
-    about: 'https://itsrakesh.co/about',
-    status: 'https://status.itsrakesh.co',
-    newsletter: 'https://newsletter.itsrakesh.co',
-    kofi: 'https://ko-fi.com/itsrakesh'
+    // about: 'https://itsrakesh.co/about',
+    // status: 'https://status.itsrakesh.co',
+    // newsletter: 'https://newsletter.itsrakesh.co',
+    // kofi: 'https://ko-fi.com/itsrakesh'
   },
   adsense: {
     publisherId: 'pub-5607528168839545',
@@ -110,7 +108,7 @@ const siteConfig = {
       inFeed: '3408517885'
     }
   },
-  copyrightText: `@ ${new Date().getFullYear()} itsrakesh. All rights reserved.`,
+  copyrightText: `@ ${new Date().getFullYear()} Blog HL Digital. Todos os direitos reservados.`,
   siteVersion: '1.0.0'
 };
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,36 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { ApolloProvider } from '@apollo/client';
+import useSSR from 'use-ssr'
+import { useState, useEffect } from 'react'
 
 import { client } from '@/utils/index.js';
 import theme from '../theme';
 import '../styles/globals.css';
 import Fonts from '../theme/Fonts';
 
+function ClientSideRenderedNextJS({ children }) {
+  const [domLoaded, setDomLoaded] = useState(false)
+  
+  useEffect(() => {
+    setDomLoaded(true)
+  }, [])
+
+  const { isServer } = useSSR()
+  if (!domLoaded) return null
+  if (isServer) return <></>
+
+  return children
+}
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ApolloProvider client={client}>
       <ChakraProvider theme={theme}>
         <Fonts />
-        <Component {...pageProps} />
+        {/* <Component {...pageProps} /> */}
+        <ClientSideRenderedNextJS>
+          <Component {...pageProps} />
+        </ClientSideRenderedNextJS>
       </ChakraProvider>
     </ApolloProvider>
   );
